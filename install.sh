6b6a870a39744d0a4888907f3fcb1f38947dec96
amidevous <amidevous@users.noreply.github.com>
2024-06-04 21:07:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,29 +39,12 @@ else
     echo "Use online actual Ubuntu LTS Version 20.04 22.04 or 24.04."
     exit 1
 fi
-if [ -f "/usr/bin/apt-get" ]; then
-apt-get update
-apt-get -y dist-upgrade
-apt-get -y install python python-dev unzip
-apt-get -y install python2 python2-dev unzip
-apt-get -y install python2.7 python2.7-dev unzip
-apt-get -y install python2.8 python2.8-dev unzip
-apt-get -y install python3 python3-dev unzip
-elif [ -f "/usr/bin/dnf" ]; then
-dnf -y update
-dnf -y  install python python-devel unzip
-dnf -y  install python2 python2-devel unzip
-dnf -y  install python2.7 python2.7-devel unzip
-dnf -y  install python2.8 python2.8-devel unzip
-dnf -y  install python3 python3-devel unzip
-elif [ -f "/usr/bin/yum" ]; then
-yum -y update
-yum -y  install python python-devel unzip
-yum -y  install python2 python2-devel unzip
-yum -y  install python2.7 python2.7-devel unzip
-yum -y  install python2.8 python2.8-devel unzip
-yum -y  install python3 python3-devel unzip
-fi
+sudo DEBIAN_FRONTEND=noninteractive apt-get update >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python python-dev unzip >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python2 python2-dev unzip >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python2.7 python2.7-dev unzip >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python2.8 python2.8-dev unzip >/dev/null 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3 python3-dev unzip >/dev/null 2>&1
 cd /root
 wget https://github.com/amidevous/xui.one/releases/download/test/XUI_1.5.12.zip -O XUI_1.5.12.zip
 unzip XUI_1.5.12.zip
